91da91ffc826f9fd4f2aba959047d82a98a86632
João Matos <joaofmatos.10@gmail.com>
2025-11-04 19:10:23 -0300

    refs #947: Cria models dependentes de proposicao.

--- src/model/proposicao/proposicao.ts
+++ src/model/proposicao/proposicao.ts
@@ -1,4 +1,3 @@
-import { Anexo, Autoria, ColegiadoApreciador, Epigrafe, OpcoesImpressao } from '../emenda/emenda';
 import { Revisao } from '../revisao/revisao';
 import { NotaRodape } from '../../components/editor-texto-rico/notaRodape';
 import { ProjetoNorma } from '../lexml/documento/projetoNorma';
@@ -29,6 +28,86 @@ export class Proposicao {
   ementa = '';
 }
 
+export const getRefProposicaoReduzida = (proposicao: Proposicao): RefProposicao => {
+  return {
+    urn: proposicao.urn,
+    sigla: proposicao.sigla,
+    numero: proposicao.numero,
+    ano: proposicao.ano,
+    ementa: proposicao.ementa,
+  };
+};
+
 export type Metadados = {
   [key: string]: string | number | boolean | string[] | number[] | boolean[];
 };
+
+// Referência reduzida da proposição ----------------------------
+export class RefProposicao {
+  urn = '';
+  sigla = '';
+  numero = '';
+  ano = '';
+  ementa = '';
+}
+
+// Colegiado apreciador da emenda ----------------------------
+export class ColegiadoApreciador {
+  siglaCasaLegislativa?: 'CN' | 'SF' | 'CD' = 'CN';
+  tipoColegiado: 'Plenário' | 'Comissão' | 'Plenário via Comissão' = 'Plenário';
+  siglaComissao?: string;
+}
+
+// Epígrafe ----------------------------
+export class Epigrafe {
+  texto = '';
+  complemento = '';
+}
+
+// Autoria ----------------------------
+export class Autoria {
+  tipo = TipoAutoria.PARLAMENTAR;
+  imprimirPartidoUF = true;
+  quantidadeAssinaturasAdicionaisSenadores = 0;
+  quantidadeAssinaturasAdicionaisDeputados = 0;
+  // TODO - Tornar opcional quando formos implementar outro tipo de autoria.
+  parlamentares: Array<Parlamentar> = [];
+  colegiado?: ColegiadoAutor;
+}
+
+// Autoria ----------------------------
+export enum TipoAutoria {
+  NAO_IDENTIFICADO = 'Não identificado',
+  PARLAMENTAR = 'Parlamentar',
+  COMISSAO = 'Comissão',
+  CASA_LEGISLATIVA = 'Casa Legislativa',
+}
+
+export class Parlamentar {
+  identificacao = '';
+  nome = '';
+  sexo: 'M' | 'F' = 'M';
+  siglaPartido = '';
+  siglaUF = '';
+  siglaCasaLegislativa: 'SF' | 'CD' = 'CD';
+  cargo = '';
+}
+
+export class ColegiadoAutor {
+  identificacao = '';
+  nome = '';
+  sigla = '';
+}
+
+// Opções de impressão -----------------------------
+export class OpcoesImpressao {
+  imprimirBrasao = true;
+  textoCabecalho = '';
+  reduzirEspacoEntreLinhas = false;
+  tamanhoFonte = 14;
+}
+
+export class Anexo {
+  nomeArquivo = '';
+  base64 = '';
+}
